05236283e38d0dadaf6d4aaf978438fc892a0e3f
Abhishek maurya <abhihsek@maurya.com>
2021-10-26 22:20:43 +0530

    change in driver

--- src/components/CustomerPages/DriverDetailAuto.js
+++ src/components/CustomerPages/DriverDetailAuto.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 import ProfileHeader from './ProfileHeader'
 
 export default class DriverDetailAuto extends Component {
-   
     render() {
         return (
             <div>
             <div class="jumbotron">
            
+            
             <h1 class="display-4">Driver For Your Drive</h1>
             <p class="lead">Driver Information As Per Auto Driver....</p>
             <hr class="my-4"></hr>
